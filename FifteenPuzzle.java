75aa13a4f408e40c79a4f7f907029ab476a7aebf
vijaykumar-s-j <sjvijaykumar27@gmail.com>
2024-10-23 13:09:01 +0530

    Add files via upload

---
+++ FifteenPuzzle.java
@@ -0,0 +1,115 @@
+import java.util.Random;
+import java.util.*;
+
+public class FifteenPuzzle {
+     int SIZE = 3;
+     int[][] board = new int[SIZE][SIZE];
+     int emptyRow, emptyCol;
+
+    public FifteenPuzzle() {
+        initializeBoard();
+        shuffleBoard();
+    }
+
+    public void initializeBoard() {
+        int value = 1;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = value++;
+            }
+        }
+        board[SIZE - 1][SIZE - 1] = 0;
+        emptyRow = SIZE - 1;
+        emptyCol = SIZE - 1;
+    }
+
+    private void shuffleBoard() {
+        Random rand = new Random();
+        for (int i = 0; i < 32; i++) {
+            int direction = rand.nextInt(4);
+            switch (direction) {
+                case 0 : move("UP"); break;
+                case 1 : move("DOWN"); break;
+                case 2 : move("LEFT"); break;
+                case 3 : move("RIGHT");break;
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == 0) {
+                    System.out.print("   ");
+                } else {
+                    System.out.printf("%2d ", board[i][j]);
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public boolean move(String direction) {
+        int newRow = emptyRow;
+        int newCol = emptyCol;
+
+        switch (direction) 
+        {
+            case "UP" : newRow--; break;
+            case "DOWN" : newRow++; break;
+            case "LEFT" : newCol--; break;
+            case "RIGHT" : newCol++; break;
+            default: 
+            {
+                System.out.println("Invalid direction! Use UP, DOWN, LEFT, RIGHT.");
+                return false;
+            }
+    
+        }
+        if (newRow >= 0 && newRow < SIZE && newCol >= 0 && newCol < SIZE) {
+            board[emptyRow][emptyCol] = board[newRow][newCol];
+            board[newRow][newCol] = 0;
+            emptyRow = newRow;
+            emptyCol = newCol;
+            return true;
+        }
+        
+        return false;
+    }
+    public boolean isSolved() {
+        int value = 1;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (i == SIZE - 1 && j == SIZE - 1) {
+                    return board[i][j] == 0;
+                }
+                if (board[i][j] != value++) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        FifteenPuzzle puzzle = new FifteenPuzzle();
+
+        System.out.println("Welcome to the 15 Puzzle Game!");
+        puzzle.printBoard();
+
+        while (!puzzle.isSolved()) {
+            System.out.print("Enter your move (UP, DOWN, LEFT, RIGHT): ");
+            String move = scanner.nextLine().toUpperCase();
+
+            if (puzzle.move(move)) {
+                puzzle.printBoard();
+            } else {
+                System.out.println("Invalid move, try again.");
+            }
+        }
+        System.out.println("Congratulations! You've solved the puzzle!");
+        scanner.close();
+    }
+}
---
+++ Mine.java
@@ -0,0 +1,181 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Mine {
+    public static final char UNOPENED = '-';
+    public static final char OPENED = '0';
+    public static final char MINE = '*';
+    public static final char FLAGGED = 'F';
+    public static final int SIZE = 9;
+    public static final int MINES = 3;
+    
+    public char[][] board;
+    public  static boolean[][] revealed;
+    public boolean[][] flagged;
+    
+    public Mine() {
+        board = new char[SIZE][SIZE];
+        revealed = new boolean[SIZE][SIZE];
+        flagged = new boolean[SIZE][SIZE];
+        initializeBoard();
+    }
+    
+    public void initializeBoard() 
+    {
+        Random rand = new Random();
+        int minesPlaced = 0;
+        
+        while (minesPlaced < MINES) {
+            int x = rand.nextInt(SIZE);
+            int y = rand.nextInt(SIZE);
+            if (board[x][y] != MINE) 
+            {
+                board[x][y] = MINE;
+                minesPlaced++;
+            }
+        }
+        for (int x = 0; x < SIZE; x++) {
+            for (int y = 0; y < SIZE; y++) {
+                if (board[x][y] != MINE) {
+                    board[x][y] = (char) ('0' + countMines(x, y));
+                }
+            }
+        }
+    }
+    public int countMines(int x, int y) {
+        int count = 0;
+        for (int i = -1; i <= 1; i++) {
+            for (int j = -1; j <= 1; j++) {
+                if (i == 0 && j == 0) continue;
+                int nx = x + i;
+                int ny = y + j;
+                if (nx >= 0 && nx < SIZE && ny >= 0 && ny < SIZE && board[nx][ny] == MINE) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+    
+    public void printBoard() {
+        System.out.print("  ");
+        for (int i = 0; i < SIZE; i++) 
+        {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        
+        for (int i = 0; i < SIZE; i++) 
+        {
+            System.out.print(i + " ");
+            for (int j = 0; j < SIZE; j++) 
+            {
+                if (flagged[i][j]) 
+                {
+                    System.out.print(FLAGGED + " ");
+                } 
+                else if (revealed[i][j]) 
+                {
+                    System.out.print(board[i][j] + " ");
+                } 
+                else 
+                {
+                    System.out.print(UNOPENED + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public void play() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            printBoard();
+            System.out.print("Enter command (o x y to open, f x y to flag): ");
+            String command = scanner.nextLine();
+            String[] parts = command.split(" ");
+            if (parts.length != 3) {
+                System.out.println("Invalid command!");
+                continue;
+            }
+            String action = parts[0];
+            int x = Integer.parseInt(parts[1]);
+            int y = Integer.parseInt(parts[2]);
+            
+            if (x < 0 || x >= SIZE || y < 0 || y >= SIZE) {
+                System.out.println("Coordinates out of bounds!");
+                continue;
+            }
+            
+            if (action.equals("o")) 
+            {
+                if (flagged[x][y]) 
+                {
+                    System.out.println("Cannot open flagged cell!");
+                    continue;
+                }
+                if (board[x][y] == MINE) 
+                {
+                    System.out.println("Game Over! You hit a mine!");
+                    revealed[x][y] = true;
+                    printBoard();
+                    break;
+                }
+                
+            } 
+            else if (action.equals("f")) 
+            {
+                flagged[x][y] = !flagged[x][y]; // Toggle flag
+            } 
+            else if(!action.equals("o")||!action.equals("f")||parts.length != 3)
+            {
+                System.out.println("Invalid command!");
+            }
+            
+                revealCell(x, y);
+                if (checkWin() == true) 
+                {
+                    printBoard();
+                    System.out.println("Congratulations! You've cleared the minefield!");
+                    break;
+                }
+            
+        }
+        scanner.close();
+    }
+    private static boolean checkWin() {
+        int revealedCount = 0;
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (revealed[i][j]) {
+                    revealedCount++;
+                }
+            }
+        }
+        if(revealedCount == SIZE * SIZE - MINES)  return true;
+        
+        return false;
+    }
+    
+    public void revealCell(int x, int y) {
+        if (x < 0 || x >= SIZE || y < 0 || y >= SIZE || revealed[x][y] || flagged[x][y]) {
+            return;
+        }
+        revealed[x][y] = true;
+        if (board[x][y] == '0') 
+        {
+            for (int i = -1; i <= 1; i++) 
+            {
+                for (int j = -1; j <= 1; j++) 
+                {
+                    if (i == 0 && j == 0) continue;
+                    revealCell(x + i, y + j);
+                }
+            }
+        }
+    }
+    
+    public static void main(String[] args) {
+        Mine game = new Mine();
+        game.play();
+    }
+}
